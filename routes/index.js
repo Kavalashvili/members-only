f134d4e97304ebfc3d791cae1d7a48f4f3e91bd3
Ivane Kavalashvili <ivanekavalashvili@gmail.com>
2023-07-04 12:53:42 -0700

    Display messages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const Message = require('../models/message');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Members Only' });
+router.get('/', async (req, res, next) => {
+  try {
+    const messages = await Message.find({})
+      .populate('author', 'first_name')
+      .exec();
+    res.render('index', { title: 'Members Only', messages: messages });
+  } catch (err) {
+    console.log(err);
+    res.redirect('/error');
+  }
 });
 
 module.exports = router;
